7559dad429e277ef50f170d237c64f9632e80e1c
nunnunhap <yew240227@gmail.com>
2024-11-02 23:07:39 +0900

    11/2 자바 두 번째 복습 시작

---
+++ Hello/src/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public String sayHello(String name) {
+        return "안녕하세요?" + name + "씨!";
+    }
+}
---
+++ Hello/src/Main.java
@@ -0,0 +1,8 @@
+// 이것은 첫번째 자바 프로그램입니다.
+public class Main {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        String message = hello.sayHello("유영");
+        System.out.println(message);
+    }
+}
